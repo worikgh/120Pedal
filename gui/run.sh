0b5350abc12785e39bc873af498dbe3ffde8de98
Worik <5387413+worikgh@users.noreply.github.com>
2025-07-29 10:08:43 +1200

    Default to inverted
    The buttons to reduce volume are hidden otherwise

--- gui/run.sh
+++ gui/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -eu
-INVERT=${1-0}
+INVERT=${1-1} # Default to true
 DIR=$(dirname "$(realpath "$0")")
 cd ${DIR}
 ($DIR/target/release/qzn3t-gui $DIR/qzn3t_mod_ui.sh $INVERT 2>&1) > /tmp/gui.log
