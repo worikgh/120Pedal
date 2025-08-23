56d5d63df4ea0cf72995abf444cb96329dcc28fd
Worik <5387413+worikgh@users.noreply.github.com>
2025-08-23 23:41:10 +1200

    Send logs to Journald

--- gui/run.sh
+++ gui/run.sh
@@ -4,4 +4,4 @@ set -eu
 INVERT=${1-1} # Default to true
 DIR=$(dirname "$(realpath "$0")")
 cd ${DIR}
-($DIR/target/release/qzn3t-gui $DIR/qzn3t_mod_ui.sh $INVERT 2>&1) > /tmp/gui.log
+($DIR/target/release/qzn3t-gui $DIR/qzn3t_mod_ui.sh $INVERT 2>&1) |systemd-cat -t qzn3t
