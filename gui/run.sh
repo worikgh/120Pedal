8cbdf00404691e59db776eb9631ed56b04cec78f
Worik <5387413+worikgh@users.noreply.github.com>
2025-10-13 16:08:38 +1300

    Specify the size of the window and remove `invert` argument

--- gui/run.sh
+++ gui/run.sh
@@ -4,4 +4,8 @@ set -eu
 INVERT=${1-1} # Default to true
 DIR=$(dirname "$(realpath "$0")")
 cd ${DIR}
-($DIR/target/release/qzn3t-gui $DIR/qzn3t_mod_ui.sh $INVERT 2>&1) |systemd-cat -t qzn3t
+
+## Width and height passed (480x250) because of a bug.  If not
+## supplied the max-width, max-height are supposed to be used.  But
+## the window is too tall and the lower row of buttons is invisible
+($DIR/target/release/qzn3t-gui $DIR/qzn3t_mod_ui.sh 480 250 2>&1) |systemd-cat -t qzn3t
