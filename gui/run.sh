5ceda464fd2bc40b4e61dd866760e8238223a70e
Worik <5387413+worikgh@users.noreply.github.com>
2025-10-19 21:34:06 +1300

    Update the GUI startup script to use a command line argument

--- gui/run.sh
+++ gui/run.sh
@@ -5,4 +5,4 @@ INVERT=${1-1} # Default to true
 DIR=$(dirname "$(realpath "$0")")
 cd ${DIR}
 
-($DIR/target/release/qzn3t-gui $DIR/qzn3t_mod_ui.sh 2>&1) |systemd-cat -t qzn3t
+($DIR/target/release/qzn3t-gui -c $DIR/qzn3t_mod_ui.sh 2>&1) |systemd-cat -t qzn3t
