c2a838207d9c523f2c917c05906f390beb8e5b1f
Worik <5387413+worikgh@users.noreply.github.com>
2025-12-26 14:51:29 +1300

    Specifically add dimensions of GUI screen on 7-inch touch screen

--- gui/run.sh
+++ gui/run.sh
@@ -6,4 +6,4 @@ DIR=$(dirname "$(realpath "$0")")
 cd ${DIR}
 
 # Specify x/y as "Fullscreen" using the SDL2 GUI library disapears off the bottom of the screen
-($DIR/target/release/qzn3t-gui -c $DIR/qzn3t_mod_ui.sh -x 480 -y 280  2>&1) |systemd-cat -t qzn3t
+($DIR/target/release/qzn3t-gui -c $DIR/qzn3t_mod_ui.sh -x 1280 -y 640  2>&1) |systemd-cat -t qzn3t
